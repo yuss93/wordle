585eb81bafdddf1e57417e4381438fa2c79b2a7c
yuss93 <yousifaldirawi@gmail.com>
2026-06-02 16:28:51 -0400

    improvments: lowercase,game over message, centered letters

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
+// word list
 const words=["apple","grape","storm","money","skill","tasks","learn","study","malik"];
-
+// game state
 let gameOver=false;
-let index = Math.floor(Math.random()*words.length);
-let secretWord=words[index]
+let index = Math.floor(Math.random()*words.length); //pick random word
+let secretWord=words[index] // one word chosen based on the index picked
 let currentRow=0;
 let input = document.getElementById("guess-input");
 let button = document.getElementById("submit-btn");
 let message = document.getElementById("message");    
 let row = document.querySelectorAll(".row");
 function handleGuess(){
-    if (currentRow>=6 || gameOver){
+    if (currentRow>=6 || gameOver){ // game over check
         if (gameOver){
             message.textContent="You already Won!";
         }
         else{
-            message.textContent="Game Over";
+            message.textContent="Game Over! The word was: "+secretWord;
         }
        return
     }
     message.textContent = "";
-    let guess =input.value
-    if (guess.length!=5){
+    let guess =input.value.toLowerCase();
+    if (guess.length!=5){ // validate input
         message.textContent = "Word must be 5 letters!";
         return;
     }
-    if (!(/^[a-zA-Z]+$/.test(guess))) {
+    if (!(/^[a-zA-Z]+$/.test(guess))) { //validate input
         message.textContent = "Only letters are allowed!";
         return;
     }
-    console.log(guess)
-    let activeRow = row[currentRow];
-    let tiles = activeRow.querySelectorAll(".tile")
-
+    let activeRow = row[currentRow]; // getting the active row
+    let tiles = activeRow.querySelectorAll(".tile") // active tile
+    input.value=""
     for (let i=0;i<5;i++){
+        
         tiles[i].textContent = guess[i];
-
+        // the color of each tile
         if (guess[i]==secretWord[i]){
             tiles[i].style.backgroundColor="green";
         }
@@ -45,12 +46,12 @@ function handleGuess(){
             tiles[i].style.backgroundColor ="grey";
         }
     }
+    // the win check
     if (guess === secretWord){
         gameOver=true
         message.textContent="You Win!";
         return;
     }
-    input.value=""
     currentRow+=1;
 }
  button.addEventListener("click", handleGuess);
