0c0fc3dc23aff6748c3ce2918811f96b64e0fda1
yuss93 <yousifaldirawi@gmail.com>
2026-06-02 16:14:28 -0400

    add game logic

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const words=["apple","grape","storm","money","skill","tasks","learn","study","malik"];
 
+let gameOver=false;
 let index = Math.floor(Math.random()*words.length);
 let secretWord=words[index]
 let currentRow=0;
 let input = document.getElementById("guess-input");
 let button = document.getElementById("submit-btn");
-let message = document.getElementById("message");
+let message = document.getElementById("message");    
+let row = document.querySelectorAll(".row");
 function handleGuess(){
+    if (currentRow>=6 || gameOver){
+        if (gameOver){
+            message.textContent="You already Won!";
+        }
+        else{
+            message.textContent="Game Over";
+        }
+       return
+    }
     message.textContent = "";
     let guess =input.value
     if (guess.length!=5){
@@ -18,5 +29,28 @@ function handleGuess(){
         return;
     }
     console.log(guess)
+    let activeRow = row[currentRow];
+    let tiles = activeRow.querySelectorAll(".tile")
+
+    for (let i=0;i<5;i++){
+        tiles[i].textContent = guess[i];
+
+        if (guess[i]==secretWord[i]){
+            tiles[i].style.backgroundColor="green";
+        }
+        else if(secretWord.includes(guess[i])){
+            tiles[i].style.backgroundColor ="yellow";
+        }
+        else{
+            tiles[i].style.backgroundColor ="grey";
+        }
+    }
+    if (guess === secretWord){
+        gameOver=true
+        message.textContent="You Win!";
+        return;
+    }
+    input.value=""
+    currentRow+=1;
 }
  button.addEventListener("click", handleGuess);
